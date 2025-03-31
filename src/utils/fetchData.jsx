4b449f2d83ab15bd3f35add03602344544a4b23e
PranavJ1612 <pranavjagdale161202@gmail.com>
2025-03-31 09:29:37 +0530

    Fixed credentials issue

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -1,18 +1,18 @@
 // 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85'
 
 export const exerciseOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'ef1ef59b89msh124413d88c3f1adp135786jsn43ed77b904da',
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_API_KEY_OF_URL,
   },
 };
 
 export const youtubeOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'ef1ef59b89msh124413d88c3f1adp135786jsn43ed77b904da',
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_API_KEY_OF_URL,
   },
 };
 
